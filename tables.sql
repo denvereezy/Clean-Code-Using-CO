34f8a476d966cea193e52d39b0320b52253061fd
denver daniels <denver@projectcodex.co>
2016-05-18 11:49:01 +0200

    table script

---
+++ tables.sql
@@ -0,0 +1,13 @@
+create table users(
+  id int auto_increment primary key,
+  username varchar(100)
+);
+
+insert into users(username) values('bobby');
+insert into users(username) values('nikki');
+insert into users(username) values('bella');
+insert into users(username) values('dolly');
+insert into users(username) values('jack');
+insert into users(username) values('donald');
+insert into users(username) values('derick');
+insert into users(username) values('martin');
