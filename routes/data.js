9b88264efa29180183eda6eb4aa3fc67e4129844
denver daniels <denver@projectcodex.co>
2016-05-18 11:20:39 +0200

    first route

---
+++ routes/data.js
@@ -0,0 +1,11 @@
+exports.showData = function(req, res, next) {
+co(function* (){
+    try{
+        var users = yield userService.findAllUsers();
+        res.render('index',users);
+    }
+    catch(err){
+        next(err);
+    };
+});
+};
