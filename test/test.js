d4deb5eb3a7bac173d08ad2ef41b23ea8b3e5d01
denver daniels <denver@projectcodex.co>
2016-05-18 13:28:19 +0200

    co module added

--- test/test.js
+++ test/test.js
@@ -1,34 +1,30 @@
 const assert = require('assert');
 const mysql = require('mysql');
+const co = require('co');
 const password = process.env.MYSQL_PWD !== null ? process.env.MYSQL_PWD : 'passw0rd';
 const UserService = require('../data-services/userService');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: process.env.MYSQL_USER || 'root',
-  password: password,
-  port: 3306,
-  database: 'travis_db'
+    host: 'localhost',
+    user: process.env.MYSQL_USER || 'root',
+    password: password,
+    port: 3306,
+    database: 'travis_db'
 });
 
 connection.connect();
 const userService = new UserService(connection)
 
-describe('testing co module', function(){
-  it('should return a list of users', function(done){
-    var userData = function(next) {
-      co(function* (){
-          try{
-              var users = yield userService.findAllUsers();
-              console.log(users);
-              return users;
-          }
-          catch(err){
-              next(err);
-          };
-      });
-    };
-    assert(users);
-    done();
-  });
+describe('testing co module', function() {
+    it('should return a list of users', function(done) {
+        co(function*() {
+            try {
+                var users = yield userService.findAllUsers();
+                assert(users);
+                done();
+            } catch (err) {
+                next(err);
+            }
+        })
+    });
 });
